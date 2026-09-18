330aea079bc15b14d16ab21405ff63b83f25d0f3
Norel <norel.evoagile@gmail.com>
2026-09-18 14:44:50 +0900

    Update nixpkgs and contexts.

--- flake.nix
+++ flake.nix
@@ -33,56 +33,54 @@
     extra-trusted-public-keys = "context-minimals.cachix.org-1:pYxyH24J/A04fznRlYbTTjWrn9EsfUQvccGMjfXMdj0=";
   };
 
-  outputs = { self, ... }@inputs:
-    let
-      forAllSystems = nixpkgs-lib.attrsets.genAttrs systems;
-      nixpkgs-lib = inputs.nixpkgs.lib;
-      systems = import inputs.systems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          packages' = self.packages.${system};
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-        in
-        {
-          luametatex = pkgs.callPackage ./pkgs/luametatex {
-            src = inputs.context + /source/luametatex;
-          };
+  outputs = {self, ...} @ inputs: let
+    forAllSystems = nixpkgs-lib.attrsets.genAttrs systems;
+    nixpkgs-lib = inputs.nixpkgs.lib;
+    systems = import inputs.systems;
+  in {
+    packages = forAllSystems (
+      system: let
+        packages' = self.packages.${system};
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        luametatex = pkgs.callPackage ./pkgs/luametatex {
+          src = inputs.context + /source/luametatex;
+        };
 
-          luatex = pkgs.callPackage ./pkgs/luatex {
-            src = inputs.luatex;
-          };
+        luatex = pkgs.callPackage ./pkgs/luatex {
+          src = inputs.luatex;
+        };
 
-          context-minimals = pkgs.callPackage ./pkgs/context-minimals {
-            inherit (inputs) context context-fonts context-modules;
-            inherit (packages') luametatex luatex;
-          };
+        context-minimals = pkgs.callPackage ./pkgs/context-minimals {
+          inherit (inputs) context context-fonts context-modules;
+          inherit (packages') luametatex luatex;
+        };
 
-          default = packages'.context-minimals;
-        });
+        default = packages'.context-minimals;
+      }
+    );
 
-      apps = forAllSystems (system:
-        let
-          apps' = self.apps.${system};
-          mkApp = bin: {
-            type = "app";
-            program = "${packages'.default}/bin/${bin}";
-          };
-          packages' = self.packages.${system};
-        in
-        {
-          context = mkApp "context";
-          luametatex = mkApp "luametatex";
-          luatex = mkApp "luatex";
-          mtxrun = mkApp "mtxrun";
-          default = apps'.context;
-        });
+    apps = forAllSystems (
+      system: let
+        apps' = self.apps.${system};
+        mkApp = bin: {
+          type = "app";
+          program = "${packages'.default}/bin/${bin}";
+        };
+        packages' = self.packages.${system};
+      in {
+        context = mkApp "context";
+        luametatex = mkApp "luametatex";
+        luatex = mkApp "luatex";
+        mtxrun = mkApp "mtxrun";
+        default = apps'.context;
+      }
+    );
 
-      lib = import ./modules/lib {
-        inherit (inputs) nixpkgs;
-        inherit forAllSystems nixpkgs-lib;
-        contextPackages = self.packages;
-      };
+    lib = import ./modules/lib {
+      inherit (inputs) nixpkgs;
+      inherit forAllSystems nixpkgs-lib;
+      contextPackages = self.packages;
     };
+  };
 }
